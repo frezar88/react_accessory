ce91a5decf334a5c8eeb17566ca64d779f996775
Tobolich Alexander <frezar2007@gmail.com>
2022-06-23 09:09:01 +0300

    manager

--- src/App.js
+++ src/App.js
@@ -1,11 +1,85 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {getData} from "./axios/requests";
+import './styles/style/style.css'
+import CardItem from "./components/CardItem";
 
 const App = () => {
+    const [data, setData] = useState()
+    useEffect(() => {
+        const url = new URL(window.location.href)
+        const link = url.searchParams.get('link')
+        getData(link).then((data) => {
+            setData(data.data['client'])
+
+        })
+    }, [])
     return (
         <div>
-            s
+            <div className={'site'}>
+                <header>
+                    <div className="container">
+                        <h5>Информация пользователя</h5>
+                    </div>
+                </header>
+
+                <main>
+                    <div className="container">
+                        <div className="content">
+                            <aside className="side-bar">
+                                <h4>Информация</h4>
+                                {
+                                    data
+                                        ? <ul>
+                                            <li>Имя: <span>{data['full_name']}</span></li>
+                                            <li>Телефон: <span>{data['phone']}</span></li>
+                                            <li>
+                                                Бренд: <span>{data['clientAccessories'][0]['accessory']['brandName']}</span>
+                                            </li>
+                                            <li>
+                                                Модель: <span>{data['clientAccessories'][0]['accessory']['modelName']}</span>
+                                            </li>
+                                        </ul>
+                                        : ''
+
+                                }
+
+                            </aside>
+                            <div style={{display:'flex',width:'100%',flexWrap:'wrap'}}>
+                                    {
+                                        data
+                                            ? data['clientAccessories'].map((el) =>
+                                            <CardItem
+                                                key={el.accessory.accessoryId}
+                                                id={el.accessory.accessoryId}
+                                                imgURL={el.accessory.accessoryPhoto}
+                                                title={el.accessory.accessoryName}
+                                                description={el.accessory.accessoryDescription}
+                                                priceDiscount={el.accessory.accessoryPriceProductDiscount}
+                                                priceProduct={el.accessory.accessoryPriceProduct}
+                                                priceWork ={el.accessory.accessoryPriceWork}
+                                                priceDiscountWork={el.accessory.accessoryPriceWorkDiscount}
+                                                discount={el.accessory.accessoryPriceProduct.replace(/\s/g, '')/el.accessory.accessoryPriceProductDiscount.replace(/\s/g, '')*100}
+                                            />
+
+                                            )
+                                            : ''
+
+                                    }
+                            </div>
+
+                        </div>
+
+                    </div>
+                </main>
+
+                <footer>
+
+                </footer>
+            </div>
         </div>
-    );
+
+    )
+        ;
 };
 
 export default App;
--- src/axios/requests.js
+++ src/axios/requests.js
@@ -1,11 +1,5 @@
 import {$host} from "./index";
 
-export const getAccessories = async (model,brand) => {
-
-    // return await $host.get(`/server/web/accessories/api?model=${model}&brand=${brand}`)
-    return await $host.get(`/server/web/accessories/api?brand=${brand}&model=${model}`)
-}
-
-export const sendDataToBack = async (data) => {
-    return await $host.post(`/server/web/accessories/api/save-user-data`,data)
+export const getData = async (id) => {
+    return await $host.get(`/server/web/accessories/api/get-client-information?link=${id}`)
 }
